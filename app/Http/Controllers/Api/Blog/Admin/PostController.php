0946b30ab208e90499b9dd4f304197dddbcbf0d4
ForStudyGit7 <andrii.rusak@oa.edu.ua>
2026-06-05 15:44:19 +0000

    Laravel lab9

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Http\Controllers\Api\Blog\Admin\BaseController;
 use App\Repositories\BlogPostRepository;
-use Illuminate\Http\Request;
+use App\Repositories\BlogCategoryRepository; // Додано
+use App\Http\Requests\BlogPostUpdateRequest; // Додано
+use Carbon\Carbon; // Додано для роботи з датами
+use Illuminate\Support\Str; // Додано для генерації slug
 
 class PostController extends BaseController
 {
 
-    public function __construct(private BlogPostRepository $blogPostRepository)
-    {
+    public function __construct(
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository
+    ) {
         // parent::__construct();
     }
 
@@ -19,9 +24,49 @@ class PostController extends BaseController
      */
     public function index()
     {
-
         $paginator = $this->blogPostRepository->getAllWithPaginate();
 
         return $paginator;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BlogPostUpdateRequest $request, $id)
+    {
+        $item = $this->blogPostRepository->getEdit($id);
+
+        if (empty($item)) {
+            return response()->json([
+                'success' => false,
+                'message' => "Запис id=[{$id}] не знайдено"
+            ], 404);
+        }
+
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+
+        if (empty($item->published_at) && !empty($data['is_published'])) {
+            $data['published_at'] = Carbon::now();
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Успішно збережено',
+                'data' => $item->refresh()
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Помилка збереження'
+            ], 400);
+        }
     }
 }
